e120b0b3fdf35db2a75daa2e2b4c66ef52299ed7
ligq <31333250@qq.com>
2019-01-06 20:16:42 +0800

    增加值对象

---
+++ echo-ddd/src/main/java/org/echo/ddd/domain/vo/IdentifieldValueObject.java
@@ -0,0 +1,22 @@
+package org.echo.ddd.domain.vo;
+
+import lombok.*;
+import org.echo.ddd.domain.IdentifiedDomainObject;
+import org.echo.ddd.domain.id.Identity;
+
+/**
+ * 具有标识的值对象(要进行资源管理)
+ *
+ * @author Liguiqing
+ * @since V1.0
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@ToString
+public abstract class IdentifieldValueObject extends ValueObject implements IdentifiedDomainObject {
+    private Identity id;
+
+}
---
+++ echo-ddd/src/main/java/org/echo/ddd/domain/vo/ValueObject.java
@@ -0,0 +1,16 @@
+package org.echo.ddd.domain.vo;
+
+import java.io.Serializable;
+
+/**
+ * 值域值对象
+ * 值对:在业务域中,具有不变性的对象
+ *
+ * @author Liguiqing
+ * @since V1.0
+ */
+
+public abstract class ValueObject implements Serializable {
+
+
+}
