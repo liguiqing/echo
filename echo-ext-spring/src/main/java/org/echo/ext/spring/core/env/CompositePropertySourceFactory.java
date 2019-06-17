adae8a797af38479a28e6a32b2b042bcf19872ba
liguiking <liguiking@gmail.com>
2019-06-17 08:39:38 +0800

    重构：增加Spring扩展组件模块

---
+++ echo-ext-spring/src/main/java/org/echo/ext/spring/core/env/CompositePropertySourceFactory.java
@@ -0,0 +1,70 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.echo.ext.spring.core.env;
+
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Properties;
+
+/**
+ * <p>
+ * CompositePropertySourceFactory support properties and yaml file
+ * </P>
+ *
+ * @author liguiqing
+ * @date 2019-06-10 20:55
+ * @since V1.0.0
+ **/
+public class CompositePropertySourceFactory extends DefaultPropertySourceFactory {
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource resource)
+            throws IOException {
+        String sourceName = Optional.ofNullable(name).orElse(resource.getResource().getFilename());
+        if (!resource.getResource().exists()) {
+            return new PropertiesPropertySource(sourceName, new Properties());
+        } else if (sourceName.endsWith(".yml") || sourceName.endsWith(".yaml")) {
+            Properties propertiesFromYaml = loadYaml(resource);
+            return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+        } else {
+            return super.createPropertySource(name, resource);
+        }
+    }
+    /**
+     * load yaml file to properties
+     *
+     * @param resource
+     * @return
+     */
+    private Properties loadYaml(EncodedResource resource) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource.getResource());
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+}
